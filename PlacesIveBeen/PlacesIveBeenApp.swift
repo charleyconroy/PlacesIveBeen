84ad76b2aeeb23a0d4368fb4519d7950dfaba962
Charles Conroy <conroycd@bc.edu>
2023-03-27 16:41:52 -0400

    Initial Commit

---
+++ PlacesIveBeen/PlacesIveBeenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesIveBeenApp.swift
+//  PlacesIveBeen
+//
+//  Created by Charley Conroy on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PlacesIveBeenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
